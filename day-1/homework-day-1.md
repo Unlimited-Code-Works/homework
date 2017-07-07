37f8e926b182f64a7e9e1cbdcb4a4f846ed89711
zjllay <13501263471@163.com>
2017-07-07 11:48:50 +0800

    modify format

--- day-1/homework-day-1.md
+++
@@ -1,3 +0,0 @@
-## homework-day-1
-
-courses.json给出了一长串json，利用python读取这个文件，并导出excel表格
--- day-1/jiangli/FormatSwitch.py
+++ day-1/jiangli/FormatSwitch.py
@@ -1,6 +1,6 @@
 import tablib
 # 返回一个文件对象
-f1 = open("/Users/jiangli/Documents/学习内容/github作业/homework/zjl/file_switch/courses.json")
+f1 = open("../jiangli/courses.json")
 # 调用文件的readline()方法
 line = f1.readline()
 while line:
@@ -11,7 +11,7 @@ print("----json switch excel----")
 # 写入到外部文件中
 output = open('file_switch.xls','wb')
 with open('file_switch','w') as switch:
-    data = tablib.Dataset().load(open('/Users/jiangli/Documents/学习内容/github作业/homework/zjl/file_switch/courses.json').read())
+    data = tablib.Dataset().load(open('../jiangli/courses.json').read())
     # # .xls为二进制文件，所以直接print会出现乱码写的是二进制文件
     switch = data.xls
     output.write(switch)
